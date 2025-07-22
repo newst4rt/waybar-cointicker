df8f42a090f0fe03af4f617c765bd349a1f14bda
newst4rt <105312719+newst4rt@users.noreply.github.com>
2025-07-22 23:19:15 +0000

    Added some extras to the Waybar tooltip – replaced urllib.request with requests – initial preparation for the KuCoin API and layout appearance for multiple exchanges – some logic changes.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-import urllib.request
+import base64
+import hashlib
+import time
+import requests
 import brotli
 import json
 import configparser
 import os
 import sys
+from urllib.parse import urlencode
+import hmac
+
 
 total_balance = {}
 total = 0
@@ -15,7 +21,7 @@ header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
     }
 
-def startup():
+def __init__():
 
     ram_access = False
     config = configparser.ConfigParser()
@@ -26,8 +32,8 @@ def startup():
         if os.path.isfile(config_path) == False:
             import shutil
             print("Setup Bitpanda Configuration\n")
-            API_KEY = str(input("API-KEY: "))
-            if not API_KEY:
+            api_key = str(input("API-KEY: "))
+            if not api_key:
                 print("Error: Invalid input.")
                 exit()
             count = 1
@@ -54,7 +60,7 @@ def startup():
             else:
                 exit()
             
-            config['DEFAULT'] = {"API_KEY": API_KEY,
+            config['DEFAULT'] = {"api_key": api_key,
                             "target_currency": target_currency,
                             "waybar": waybar}
             with open('config_bitpanda.ini', 'w') as configfile:
@@ -70,12 +76,12 @@ def startup():
     else:
         config.read(config_path)
         
-    API_KEY = config['DEFAULT']['API_KEY']
+    api_key = config['DEFAULT']['api_key']
     target_currency = config['DEFAULT']['target_currency']
     waybar = config['DEFAULT']['waybar']
-    header["X-Api-Key"] = API_KEY
+    header["X-Api-Key"] = api_key
     
-    return [str(API_KEY), str(target_currency), bool(waybar)]
+    return [str(api_key), str(target_currency), bool(waybar)]
 
 def reset():
     if os.path.isfile("/dev/shm/config_bitpanda.ini") == True:
@@ -93,112 +99,169 @@ def update():
     exit()
 
 
-def create_request(site):
-    request_params = urllib.request.Request(
-        url=site, 
-        headers=header
-    )
-    req_url = urllib.request.urlopen(request_params).read()
-    decode_data = brotli.decompress(req_url)
-    json_data = json.loads(decode_data)
+def create_request(session, endpoint, path, method, signer = ""):
+
+    full_path = f"{endpoint}{path}?{urlencode}"
+    raw_url = f"{path}?{urlencode}"
+    request = requests.Request(method=method, url=full_path).prepare()
+    
+    if signer:
+        payload = method + raw_url
+        headers = signer.headers(payload)
+        request.headers.update(headers)
+    else:
+        request.headers.update(header)
+
+    send_request = session.send(request)
+    json_data = json.loads(send_request.content)
     return json_data
 
-def parse_fiat_wallet(json_data):
-    total_balance["fiat_wallet"] = {}
-    for item in json_data["data"]:
-        if float(item["attributes"]["balance"]) != 0.0 and item:
-            total_balance["fiat_wallet"][item["attributes"]["fiat_symbol"]] = item["attributes"]["balance"]
+class Bitpanda:
+    def __init__(self, session, api_key, target_currency, waybar):
+        total_balance = {}
+        self.total = 0
+        json_asset_wallet = create_request(session, "https://api.bitpanda.com", "/v1/asset-wallets", "GET")
+        json_fiat_wallet = create_request(session, "https://api.bitpanda.com", "/v1/fiatwallets", "GET")
+        json_ticker = create_request(session, "https://api.bitpanda.com", "/v2/ticker", "GET")
+        json_ticker = self.prepare_ticker(json_ticker)
+        self.parse_fiat_wallet(json_fiat_wallet, total_balance)
+        self.parse_asset_wallet(json_asset_wallet, total_balance)
+        
+        self.waybar_tooltip = {}
+        self.wcs_space = 0
+        self.wvar_space = 0
 
-def prepare_ticker(json_data):
-    json_ticker["EUR"] = json_ticker["BCPEUR"]
-    json_ticker["USD"] = json_ticker["BCPUSD"]
-    json_ticker["GBP"] = json_ticker["BCPGBP"]
-    for x in currencies[3:]:
-        json_ticker[x] = {}
-        value = 1/float(json_ticker["USD"][x])
-        json_ticker[x]["USD"] = value
-        for y in currencies[1:]:
-            y_value = float(json_ticker["USD"][y])*value
-            json_ticker[x][y] = "%.4f" % round(y_value, 4)
+        for k, v in total_balance.items():
+            if v:
+                for kx, vx in v.items():
+                    try:
+                        exchange_rate = json_ticker[kx][target_currency]
+                        if debug == True:
+                            print(f'{kx}:{float(vx) * float(exchange_rate)}')
+                        if waybar == True:
+                            self.waybar_tooltip[kx] = ["%.2f" % round(float(vx) * float(exchange_rate), 2), exchange_rate]
+                            if len(kx) > self.wcs_space:
+                                self.wcs_space = len(kx)
+                            if len(self.waybar_tooltip[kx][0]) > self.wvar_space:
+                                self.wvar_space = len(self.waybar_tooltip[kx][0])
+                            
+                    except:
+                        print(f"Error: „{target_currency}\" currency not available.")
+                        exit()
 
+                    self.total += float(vx) * float(exchange_rate) 
 
-    return json_data
+    def parse_fiat_wallet(self, json_data, total_balance):
+        total_balance["fiat_wallet"] = {}
+        for item in json_data["data"]:
+            if float(item["attributes"]["balance"]) != 0.0 and item:
+                total_balance["fiat_wallet"][item["attributes"]["fiat_symbol"]] = item["attributes"]["balance"]
 
-def parse_asset_wallet(json_data):
-    for k, v in json_data["data"]["attributes"].items():
-        total_balance[k] = {}
-        if k == "cryptocoin":
-            for x in v["attributes"]["wallets"]:
-                if float(x["attributes"]["balance"]) != 0.0 and v:
-                    #print(float(x["attributes"]["balance"]))
-                    total_balance[k][x["attributes"]["cryptocoin_symbol"]] = x["attributes"]["balance"]
-        if k == "commodity":
-            for x in v["metal"]["attributes"]["wallets"]:
-                if float(x["attributes"]["balance"]) != 0.0 and v:
-                    total_balance[k][x["attributes"]["cryptocoin_symbol"]] = x["attributes"]["balance"]
-        if k == "index":
-            for x in v["index"]["attributes"]["wallets"]:
-                if float(x["attributes"]["balance"]) != 0.0 and v:
-                    total_balance[k][x["attributes"]["cryptocoin_symbol"]] = x["attributes"]["balance"]
-        if k == "security" or k == "equity_security":
-            for y in v.keys():
-                for x in v[y]["attributes"]["wallets"]:
+    def prepare_ticker(self, json_ticker):
+        json_ticker["EUR"] = json_ticker["BCPEUR"]
+        json_ticker["USD"] = json_ticker["BCPUSD"]
+        json_ticker["GBP"] = json_ticker["BCPGBP"]
+        for x in currencies[3:]:
+            json_ticker[x] = {}
+            value = 1/float(json_ticker["USD"][x])
+            json_ticker[x]["USD"] = value
+            for y in currencies[1:]:
+                y_value = float(json_ticker["USD"][y])*value
+                json_ticker[x][y] = "%.4f" % round(y_value, 4)
+
+        return json_ticker
+    
+    def parse_asset_wallet(self, json_data, total_balance):
+        for k, v in json_data["data"]["attributes"].items():
+            total_balance[k] = {}
+            if k == "cryptocoin":
+                for x in v["attributes"]["wallets"]:
+                    if float(x["attributes"]["balance"]) != 0.0 and v:
+                        #print(float(x["attributes"]["balance"]))
+                        total_balance[k][x["attributes"]["cryptocoin_symbol"]] = x["attributes"]["balance"]
+            if k == "commodity":
+                for x in v["metal"]["attributes"]["wallets"]:
                     if float(x["attributes"]["balance"]) != 0.0 and v:
                         total_balance[k][x["attributes"]["cryptocoin_symbol"]] = x["attributes"]["balance"]
+            if k == "index":
+                for x in v["index"]["attributes"]["wallets"]:
+                    if float(x["attributes"]["balance"]) != 0.0 and v:
+                        total_balance[k][x["attributes"]["cryptocoin_symbol"]] = x["attributes"]["balance"]
+            if k == "security" or k == "equity_security":
+                for y in v.keys():
+                    for x in v[y]["attributes"]["wallets"]:
+                        if float(x["attributes"]["balance"]) != 0.0 and v:
+                            total_balance[k][x["attributes"]["cryptocoin_symbol"]] = x["attributes"]["balance"]
 
 
-if __name__ == "__main__":
-
-    if len(sys.argv) > 1:
-        if sys.argv[1].lower() == "reset":
-            reset()
-        elif sys.argv[1].lower() == "udate":
-            update()
-
-    API_KEY, target_currency, waybar = startup()
-
-    json_asset_wallet = create_request("https://api.bitpanda.com/v1/asset-wallets")
-    json_fiat_wallet = create_request("https://api.bitpanda.com/v1/fiatwallets")
-    json_ticker = create_request("https://api.bitpanda.com/v2/ticker")
-    json_ticker = prepare_ticker(json_ticker)
-    parse_fiat_wallet(json_fiat_wallet)
-    parse_asset_wallet(json_asset_wallet)
-
+def prepare_waybar_tooltip(name, total, waybar_tooltip, waybar, wcs_space, wvar_space):
     if waybar == True:
-        waybar_tooltip = {}
-        wcs_space = 0
-        wvar_space = 0
-
-    for k, v in total_balance.items():
-        if v:
-            for kx, vx in v.items():
-                try:
-                    exchange_rate = json_ticker[kx][target_currency]
-                    if debug == True:
-                        print(f'{kx}:{float(vx) * float(exchange_rate)}')
-                    if waybar == True:
-                        waybar_tooltip[kx] = ["%.2f" % round(float(vx) * float(exchange_rate), 2), exchange_rate]
-                        if len(kx) > wcs_space:
-                            wcs_space = len(kx)
-                        if len(waybar_tooltip[kx][0]) > wvar_space:
-                            wvar_space = len(waybar_tooltip[kx][0])
-                        
-                except:
-                    print(f"Error: „{target_currency}\" currency not available.")
-                    exit()
-
-                total += float(vx) * float(exchange_rate) 
-
-    if waybar == True:
-        wstring_tooltip = ""
+        title_space = (int(wcs_space) + int(wvar_space) - 10) * " "
+        wstring_tooltip = "<b>" + name + "</b>" + title_space + "<i><span color=\'gray\' rise=\'-1000\'>Total: " + str(round(total, 2)) + " " + target_currency + "</span></i>\n\n"
         sorted_waybar_tooltip = dict(sorted(waybar_tooltip.items(), key=lambda item: float(item[1][0]), reverse=True))
         for key, value in sorted_waybar_tooltip.items():
             cs_spaces = (wcs_space - len(key)) * " "
             var_spaces = (wvar_space - len(value[0])) * " "
             wstring_tooltip += f'{key}{cs_spaces} : {value[0]} {target_currency}{var_spaces} <span color=\'GreenYellow\' font-family=\'OpenSymbol\'>@</span> {value[1]}\n'
         output_text = f'₿ : {round(total, 2)} {target_currency}'
-        test_dic = {"text":output_text,"tooltip":wstring_tooltip,"class":"bitpanda"}
-        print(json.dumps(test_dic))
+        output_dic = {"text":output_text,"tooltip":wstring_tooltip, "class":name}
+        print(json.dumps(output_dic))
     else:
         print(f'₿ : {round(total, 2)} {target_currency}')
+
+class KuCoin:
+    def __init__(self, api_key: str, api_secret: str, api_passphrase: str):
+        """
+        KcSigner contains information about ‘apiKey’, ‘apiSecret’, ‘apiPassPhrase’
+        and provides methods to sign and generate headers for API requests.
+        """
+        self.api_key = api_key or ""
+        self.api_secret = api_secret or ""
+        self.api_passphrase = api_passphrase or ""
+
+        if api_passphrase and api_secret:
+            self.api_passphrase = self.sign(api_passphrase.encode('utf-8'), api_secret.encode('utf-8'))
+
+        if not all([api_key, api_secret, api_passphrase]):
+            print("API token is empty. Access is restricted to public interfaces only.")
+
+    def sign(self, plain: bytes, key: bytes) -> str:
+        hm = hmac.new(key, plain, hashlib.sha256)
+        return base64.b64encode(hm.digest()).decode()
+
+    def headers(self, plain: str) -> dict:
+        """
+        Headers method generates and returns a map of signature headers needed for API authorization
+        It takes a plain string as an argument to help form the signature. The outputs are a set of API headers.
+        """
+        timestamp = str(int(time.time() * 1000))
+        signature = self.sign((timestamp + plain).encode('utf-8'), self.api_secret.encode('utf-8'))
+
+        return {
+            "Content-Type": "application/json",
+            "KC-API-KEY": self.api_key,
+            "KC-API-PASSPHRASE": self.api_passphrase,
+            "KC-API-TIMESTAMP": timestamp,
+            "KC-API-SIGN": signature,
+            "KC-API-KEY-VERSION": "2"
+        }
+
+if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        if sys.argv[1].lower() == "reset":
+            reset()
+        elif sys.argv[1].lower() == "update":
+            update()
+
+    api_key, target_currency, waybar = __init__()
+    
+    signer = KuCoin("687ff1a9dffe710001e65ce3", "35106d0f-02ec-4356-b840-532b4cd0e7b2", "Ue.E$\\gT)\\i&vS[os\\ln")
+
+    session = requests.Session()
+    bp_data = Bitpanda(session, api_key, target_currency, waybar)
+    ku_data = create_request(session, "https://api.kucoin.com", "/api/v1/accounts/", "GET", signer)
+    #print(test)
+    if waybar == True:
+        prepare_waybar_tooltip("Bitpanda", bp_data.total, bp_data.waybar_tooltip, waybar, bp_data.wcs_space, bp_data.wvar_space)
 
